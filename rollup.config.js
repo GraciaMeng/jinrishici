88c92cd1ef633e9828790192d7cb644e6b7838f9
MengJiaXi <1040616435@qq.com>
2023-08-11 21:07:25 +0800

    feat: add type when build

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,28 @@
+import { defineConfig } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
+import { existsSync } from 'node:fs'
+import path from 'node:path'
+import { rimrafSync } from 'rimraf'
 
-export default {
-  input: './src/index.ts',
-  output: [
-    {
-      format: 'cjs',
-      file: 'dist/bundle.cjs.js',
-    },
-    {
-      format: 'es',
-      file: 'dist/bundle.esm.js'
-    }
-  ],
-  plugins: [
-    typescript()
-  ]
-}
+export default defineConfig(() => {
+  const distPath = path.resolve(__dirname, './dist')
+  if (existsSync(distPath)) {
+    rimrafSync(distPath)
+  }
+  return [{
+    input: './src/index.ts',
+    output: [
+      {
+        format: 'cjs',
+        file: 'dist/bundle.cjs.js',
+      },
+      {
+        format: 'esm',
+        file: 'dist/bundle.esm.js'
+      }
+    ],
+    plugins: [
+      typescript({ declaration: true }),
+    ],
+  }]
+})
